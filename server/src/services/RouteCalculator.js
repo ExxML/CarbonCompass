d428f413ab569a5157b08235545c217dea4243b9
Timothy Mai <timothymai7513@gmail.com>
2025-10-04 14:37:08 -0700

    add boilerplate javascript classes for back-end

---
+++ server/src/services/RouteCalculator.js
@@ -0,0 +1,22 @@
+import Route from '../models/Route.js';
+import CarbonCalculator from './CarbonCalculator.js';
+
+class RouteCalculator {
+    constructor(routeDataSource) {
+        this.routeDataSource = routeDataSource; // e.g., API object
+        this.carbonCalculator = new CarbonCalculator();
+    }
+
+    calculateRoutes(request) {
+        const rawRoutes = this.routeDataSource.getRoutes(request.origin, request.destination);
+
+        return rawRoutes.map(r => new Route(
+            r.path,
+            r.distance,
+            r.duration,
+            this.carbonCalculator.calculate(r, request.getUserPreferences().transportModes[0])
+        ));
+    }
+}
+
+export default RouteCalculator;
