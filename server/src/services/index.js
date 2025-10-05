c4af091b901b2487bbfc868f5782b141819d2c1f
vvictort <vvictort20@gmail.com>
2025-10-05 00:51:11 -0700

    hard reset to square 0

--- server/src/services/index.js
+++
@@ -1,5 +0,0 @@
-// Export all services for easy importing
-export { default as CarbonCalculator } from "./CarbonCalculator.js";
-export { default as RouteCalculator } from "./RouteCalculator.js";
-export { default as RouteOptimizationService } from "./RouteOptimizationService.js";
-export { default as MapsService } from "./MapsService.js";
