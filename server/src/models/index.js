248d568709544719b4f00ed11213c287ec5187a6
ExxML <andywu9509@gmail.com>
2025-10-04 22:05:41 -0700

    Fixed comments in index.js

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,8 +2,6 @@ import Route from './Route.js';
 
 /**
  * RouteRequest - Simple validation schema for route requests
- * This is a basic object structure - in a real application you might use
- * a more sophisticated validation library like Joi or Zod
  */
 const RouteRequest = {
     origin: '',
