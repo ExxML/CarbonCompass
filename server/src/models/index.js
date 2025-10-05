4b35b01dfc1aca54c11379ed10b6add4edbb4fc7
ExxML <andywu9509@gmail.com>
2025-10-04 20:48:19 -0700

    Deleted unused boilerplate code

--- server/src/models/index.js
+++
@@ -1,5 +0,0 @@
-// Export all models for easy importing
-export { default as User } from './User.js';
-export { default as UserPreferences } from './UserPreferences.js';
-export { default as Route } from './Route.js';
-export { default as RouteRequest } from './RouteRequest.js';
